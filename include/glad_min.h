fdbe1de020e2d0c4050c4c6ae54d1c956048523e
zishaan1911 <iamzishaan@gmail.com>
2026-07-15 08:02:33 +0000

    gl: add the GL enum constants the renderer needs

--- include/glad_min.h
+++ include/glad_min.h
@@ -35,3 +35,42 @@ typedef char           GLchar;
 typedef ptrdiff_t      GLintptr;
 typedef ptrdiff_t      GLsizeiptr;
 
+// --- Enums we need -----------------------------------------------------------
+#define GL_FALSE                            0
+#define GL_TRUE                             1
+#define GL_NO_ERROR                         0
+#define GL_TRIANGLES                        0x0004
+#define GL_FLOAT                            0x1406
+#define GL_RENDERER                         0x1F01
+#define GL_VERSION                          0x1F02
+#define GL_RGBA                             0x1908
+#define GL_RGBA32F                          0x8814
+#define GL_TEXTURE_2D                       0x0DE1
+#define GL_TEXTURE_MAG_FILTER               0x2800
+#define GL_TEXTURE_MIN_FILTER               0x2801
+#define GL_TEXTURE_WRAP_S                   0x2802
+#define GL_TEXTURE_WRAP_T                   0x2803
+#define GL_LINEAR                           0x2601
+#define GL_NEAREST                          0x2600
+#define GL_CLAMP_TO_EDGE                    0x812F
+#define GL_TEXTURE0                         0x84C0
+#define GL_COLOR_BUFFER_BIT                 0x00004000
+#define GL_VERTEX_SHADER                    0x8B31
+#define GL_FRAGMENT_SHADER                  0x8B30
+#define GL_COMPILE_STATUS                   0x8B81
+#define GL_LINK_STATUS                      0x8B82
+#define GL_INFO_LOG_LENGTH                  0x8B84
+#define GL_READ_ONLY                        0x88B8
+#define GL_WRITE_ONLY                       0x88B9
+#define GL_READ_WRITE                       0x88BA
+#define GL_FRAMEBUFFER                      0x8D40
+#define GL_READ_FRAMEBUFFER                 0x8CA8
+#define GL_DRAW_FRAMEBUFFER                 0x8CA9
+#define GL_COLOR_ATTACHMENT0                0x8CE0
+#define GL_FRAMEBUFFER_COMPLETE             0x8CD5
+#define GL_RGB                              0x1907
+#define GL_UNSIGNED_BYTE                    0x1401
+#define GL_PACK_ALIGNMENT                   0x0D05
+#define GL_UNPACK_ALIGNMENT                 0x0CF5
+#define GL_FRAMEBUFFER_SRGB                 0x8DB9
+
